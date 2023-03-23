2f8cc9d1c724d539992d5fafc3dca2e66cc6a935
Melanie Verna <kverna@vt.edu>
2023-03-23 00:09:14 -0400

    made base.py return a list of recipe names instead of a list of recipes

--- backend/base.py
+++ backend/base.py
@@ -8,6 +8,25 @@ client = MongoClient('localhost', 27017)
 db = client['recipe_db']
 recipes = db['recipes']
 
+recipes.delete_many({}) # delete all entries in database
+
+recipes.insert_one({'name':'Meatballs',
+                'time_mins':45,
+                'energy':'moderate',
+                'type':'dinner',
+                })
+
+recipes.insert_one({'name':'Veggieballs',
+                'time_mins':20,
+                'energy':'jules',
+                'type':'side',
+                })
+
+recipes.insert_one({'name':'Meat Pie',
+                'time_mins':90,
+                'energy':'big dick',
+                'type':'dinner',
+                })
 
 @app.route("/query", methods=['POST'])
 def index():
@@ -23,5 +42,5 @@ def search_by_name(query):
     list_of_matches = []
     regx = re.compile(".*" + re.escape(query), re.IGNORECASE)
     for entry in recipes.find({"name": regx}):
-        list_of_matches.append(entry)
+        list_of_matches.append(entry["name"])
     return list_of_matches
